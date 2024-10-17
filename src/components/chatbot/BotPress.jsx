99d4f1e1db805afb0cbba0192412d4c73f3c043e
project2479 <jaywrsnp6@gmail.com>
2024-10-17 15:46:29 +0800

    new

--- src/components/chatbot/BotPress.jsx
+++ src/components/chatbot/BotPress.jsx
@@ -1,4 +1,5 @@
-function BotPress() {	return (	<>
+function BotPress() {	return (
+	<>
 		<iframe
 			className="h-screen mt-8 pb-8"
 			srcDoc="<body><script src='https://cdn.botpress.cloud/webchat/v0/inject.js'></script>
@@ -7,10 +8,10 @@ function BotPress() {	return (	<>
                   'composerPlaceholder': 'Talk with me, Im always here',
                   'botConversationDescription': 'This chatbot was built surprisingly fast with Botpress',
                   'botName': 'Anxiety Ally',
-                  'botId': 'b61afab5-2894-44d5-abee-1222a12f0de1',
+                  'botId': '4ace55c4-0288-43b9-8c4e-e3c801599a7e',
                   'hostUrl': 'https://cdn.botpress.cloud/webchat/v0',
                   'messagingUrl': 'https://messaging.botpress.cloud',
-                  'clientId': 'b61afab5-2894-44d5-abee-1222a12f0de1',
+                  'clientId': '4ace55c4-0288-43b9-8c4e-e3c801599a7e',
                   'enableConversationDeletion': true,
                   'showPoweredBy': false,
                   'className': 'webchatIframe',
@@ -22,7 +23,7 @@ function BotPress() {	return (	<>
                   'closeOnEscape': false,
                   'showConversationsButton': true,
                   'enableTranscriptDownload': false,
-                  'stylesheet':'https://webchat-styler-css.botpress.app/prod/code/8315adcf-40cb-42f5-aa58-1d3f73baea43/v29249/style.css'
+                  'stylesheet':'https://webchat-styler-css.botpress.app/prod/code/8315adcf-40cb-42f5-aa58-1d3f73baea43/v11849/style.css'
                   
               });
             window.botpressWebChat.onEvent(function () { window.botpressWebChat.sendEvent({ type: 'show' }) }, ['LIFECYCLE.LOADED']);
