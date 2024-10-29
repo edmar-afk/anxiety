b68d15d8276fa4e3116c1bff399f895f63ef9d8b
project2479 <jaywrsnp6@gmail.com>
2024-10-29 08:59:38 +0800

    new

--- src/components/chatbot/Receiver.jsx
+++ src/components/chatbot/Receiver.jsx
@@ -7,7 +7,7 @@ function Receiver({message}) {
 					<div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
 						<span className="text-xs font-bold text-blue-600">Anxiety Ally</span>
 						<div>
-							<span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300/80 text-gray-600">
+							<span className="px-4 font-bold py-2 rounded-lg inline-block rounded-bl-none bg-gray-300/80 text-gray-600">
 								{message}
 							</span>
 						</div>
