5c3486e93a37fc9cb866133ab9d8f60a2131f20a
project2479 <jaywrsnp6@gmail.com>
2024-12-09 21:17:51 +0800

    new

--- src/components/chatbot/Choices.jsx
+++ src/components/chatbot/Choices.jsx
@@ -20,7 +20,7 @@ function Choices({ handleQuestionClick }) {
 						<p
 							key={question.id}
 							onClick={() => handleQuestionClickWithHide(question)}
-							className="text-white bg-blue-600 my-1 px-1.5 rounded-sm py-0.5 text-xs mx-2 cursor-pointer hover:bg-blue-700 transition">
+							className="text-white bg-blue-900 my-1 px-1.5 rounded-sm py-0.5 text-xs mx-2 cursor-pointer hover:bg-blue-700 transition">
 							{question.question}
 						</p>
 					)
